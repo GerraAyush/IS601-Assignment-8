623fcf5e90c5f2d02cc70f70b2edee716544c20d
GerraAyush <ayushgerra3@gmail.com>
2026-03-13 15:21:40 -0400

    fixed e2e tests

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,4 +1,5 @@
 import pytest
+from playwright.sync_api import expect
 
 
 @pytest.mark.e2e
@@ -7,20 +8,24 @@ def test_hello_world(page, fastapi_server):
     
     assert page.inner_text('h1') == 'Hello World'
 
+
 @pytest.mark.e2e
 def test_calculator_add(page, fastapi_server):
-    page.goto('http://localhost:8000')
-    page.fill('#a', '10')
-    page.fill('#b', '5')
+    page.goto("http://localhost:8000")
+
+    page.fill("#a", "10")
+    page.fill("#b", "5")
     page.click('button:text("Add")')
-    
-    assert page.inner_text('#result') == 'Calculation Result: 15'
+
+    expect(page.locator("#result")).to_have_text("Calculation Result: 15")
+
 
 @pytest.mark.e2e
 def test_calculator_divide_by_zero(page, fastapi_server):
-    page.goto('http://localhost:8000')
-    page.fill('#a', '10')
-    page.fill('#b', '0')
+    page.goto("http://localhost:8000")
+
+    page.fill("#a", "10")
+    page.fill("#b", "0")
     page.click('button:text("Divide")')
-    
-    assert page.inner_text('#result') == 'Error: Cannot divide by zero!'
+
+    expect(page.locator("#result")).to_have_text("Error: Cannot divide by zero!")
